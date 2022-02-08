f0d15595709c0e645fffaa313e181c23f11ed9ca
Björn Birath <bjobi903@student.liu.se>
2022-02-08 22:22:00 +0100

    add stubs for profiler

--- src/render_loop.cpp
+++ src/render_loop.cpp
@@ -7,6 +7,7 @@
 #include <imgui.h>
 #include <imgui_impl_opengl3.h>
 #include <imgui_impl_sdl.h>
+#include <easy/profiler.h>
 
 namespace gravity {
 
---
+++ utils/stubs/easy/profiler.h
@@ -0,0 +1,12 @@
+#ifndef EASY_PROFILER_H
+#define EASY_PROFILER_H
+
+#define EASY_END_BLOCK
+#define EASY_BLOCK(name, ...)
+# define EASY_FUNCTION(...)
+
+namespace profiler {
+    inline void startListen() { }
+}
+
+#endif
